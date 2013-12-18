b6b1b932d12ec2a4900c0e06628ccd90f3fbf0c3
Jacky Chen <jingchen@cmu.edu>
2013-12-18 14:24:48 -0800

    update crypt1.cpp

--- src/crypt1.cpp
+++ src/crypt1.cpp
@@ -5,102 +5,39 @@ LANG: C++
 */
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-
+#include <map>
 using namespace std;
 
-int judge(int *digital, int number, int n)
-{
-	int i, flag = 0;
-	int a, b, c, d;
-	if(n>999)
-	{
-		d = n/1000;
-		n%=1000;
-		for(i=0; i<number; i++)
-		{
-			if(d == digital[i]) flag = 1;
-		}
-		if(!flag) 
-		{
-			return flag;
-		}
-	}
-	
-	a = n/100;
-	n%=100;
-	b = n/10;
-	c = n%10;
-	flag = 0;
-	for(i=0; i<number; i++)
-	{
-		if(a == digital[i]) flag = 1;
-	}
-	if(!flag) 
-	{
-		return flag;
-	}
-	flag = 0;
-	for(i=0; i<number; i++)
-	{
-		if(b == digital[i]) flag = 1;
-	}
-	if(!flag) 
-	{
-		return flag;
-	}
-	flag = 0;
-	for(i=0; i<number; i++)
-	{
-		if(c == digital[i]) flag = 1;
-	}
-	return flag;
+map<int, bool> m;
+//hash搜索法判断一个数是否属于集合
+bool judge(int res, int a, int b) {
+	return (m[res%10]&&m[(res/10)%10]&&m[(res/100)%10]&&m[res/1000]
+			&&m[a%10]&&m[(a/10)%10]&&m[a/100]
+			&&m[b%10]&&m[(b/10)%10]&&m[b/100]);
 }
-
 int main() {
-    ofstream fout ("crypt1.out");
-    ifstream fin ("crypt1.in");
-    int number, i, j, k, p, q;
-	int *digital;
-	int a, b, c, d, e, max_c, max_d, max_e, num=0;
-	fin >> number;
-	digital = (int *)malloc(number*sizeof(int));
-	for(i=0; i<number; i++)
-    {
-    	fin >> digital[i];
+    freopen("crypt1.out", "w", stdout);
+    freopen("crypt1.in", "r", stdin);
+	int N, num = 0;
+	cin >> N;
+	for(int i=0; i<N; i++) {
+		int tmp;
+		cin >> tmp;
+		m[tmp] = true;
 	}
-	sort(digital, digital+number);
-	
-	max_d = max_c = (digital[number-1]*100) +(digital[number-1]*10) + digital[number-1];
-		
-	max_e = (digital[number-1]*1000) + (digital[number-1]*100)
-		       	+(digital[number-1]*10) + digital[number-1];
-
-	for(i=0; i<number; i++)
-	for(j=0; j<number; j++)
-	for(k=0; k<number; k++)
-	for(p=0; p<number; p++)
-	for(q=0; q<number; q++)
-	{
-		a = digital[i]*100 + digital[j]*10 + digital[k];
-		b = digital[p]*10 + digital[q];
-		c = digital[q]*a;
-		d = digital[p]*a;
-		e = a*b;
-		if((e>max_e)||(c>max_c)||digital[p]*a>max_d)
-		{
-			continue;
-		}
-		if(judge(digital, number, c)&&judge(digital, number, d)&&judge(digital, number, e))
-		{
-			num++;
-			//fout << "c = " << c << endl;
-			//fout << "d = " << d << endl;
-			//fout << "e = " << e << endl;
+	for(int x=100; x<=999; x++) {
+		for(int y=10; y<=99; y++) {
+			int a, b, res;
+			if(m[x%10]&&m[(x/10)%10]&&m[x/100]
+				&&m[y%10]&&m[y/10]) {
+				res = x*y; 
+				a = (y%10)*x; 
+				b = (y/10)*x;
+				//若res大于4位数或ab大于3位数则y无需再试
+				if(res > 9999 || a > 999 || b > 999 || a < 100 || b < 100) continue; 
+				if(judge(res, a, b)) num++;
+			}
 		}
 	}
-	
-	fout << num << endl;
-	
-    return 0;
+	cout << num << endl;
 }
