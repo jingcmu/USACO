c39a5e30ed72dbc6d8d51d1f5e9619d358f298a7
Jacky Chen <jingchen@cmu.edu>
2013-12-17 20:54:04 -0800

    update milk.cpp

--- src/milk.cpp
+++ src/milk.cpp
@@ -5,56 +5,41 @@ LANG: C++
 */
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-typedef struct 
-{
+typedef struct {
 	int price;
 	int amount;
 } farmer ;
 
-int cmp(farmer f1, farmer f2)
-{
-
-	if(f1.price < f2.price)
-	{
-		return 1;
-	}
-	else return 0;
+int cmp(farmer f1, farmer f2) {
+	return f1.price < f2.price;
 }
 
 int main() {
-    ofstream fout ("milk.out");
-    ifstream fin ("milk.in");
-    int N, M, i, P, A;
-	long total = 0, amount_1  = 0;
+    freopen("milk.out", "w", stdout);
+    freopen("milk.in", "r", stdin);
+    int total_milk, M, i, P, A;
+	long total_price = 0, amount  = 0;
 	vector<farmer> v_farmer;
-	farmer far;
-    fin >> N >> M;
-	for(i=0; i<M; i++)
-	{
-		fin >> P >> A;
-		far.price = P;
-		far.amount = A;
-		v_farmer.push_back(far);
+	farmer f;
+    cin >> total_milk >> M;
+	for(i=0; i<M; i++) {
+		cin >> P >> A;
+		f.price = P;
+		f.amount = A;
+		v_farmer.push_back(f); //把所有farmer放入vector
 	}
-	sort(v_farmer.begin(), v_farmer.end(), cmp);
-
-	for(i=0; amount_1 < N; i++)
-	{
-		total += v_farmer[i].price * v_farmer[i].amount;
-		amount_1 += v_farmer[i].amount;
+	sort(v_farmer.begin(), v_farmer.end(), cmp); //对farmer进行排序
+	for(i=0; amount < total_milk; ++i) {
+		total_price += v_farmer[i].price * v_farmer[i].amount;
+		amount += v_farmer[i].amount;
 	}
-	i--;
-	amount_1 = amount_1-N;
-	if(amount_1>0)
-	{
-		total -= v_farmer[i].price * amount_1;
+	if(amount - total_milk > 0) {
+		total_price -= v_farmer[--i].price * (amount - total_milk);
 	}
-	fout << total << endl;
+	cout << total_price << endl;
     return 0;
 }
