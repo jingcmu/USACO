77fec69aefc9b3d671121c46433c8327997c481e
Jacky Chen <jingchen@cmu.edu>
2013-12-19 18:45:41 -0800

    update milk3

--- src/milk3.cpp
+++ src/milk3.cpp
@@ -6,116 +6,88 @@ LANG: C++
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-
+#include <map>
 using namespace std;
 
-typedef struct
-{
+typedef struct {
 	int capacity;
 	int milk;
-}milk_bucket;
+} milk_bucket;
 
-int status[8000];
-int len = 0;
 int c_milk[20];
 int len_cmilk=0;
+map<int, bool> m;
 
-void pour_fromXtoY(milk_bucket *X, milk_bucket *Y)
-{
-	if((*X).milk<=(*Y).capacity-(*Y).milk)
-	{					
+void pour_fromXtoY(milk_bucket *X, milk_bucket *Y){
+	if((*X).milk<=(*Y).capacity-(*Y).milk){					
 		(*Y).milk += (*X).milk;
 		(*X).milk = 0;
 	}
-	else
-	{
+	else{
 		(*X).milk -= ((*Y).capacity-(*Y).milk);
 		(*Y).milk = (*Y).capacity;
 	}
-	return;
 }
 
-void pour_milk(milk_bucket A, milk_bucket B, milk_bucket C)
-{
-	int flag, status_l;
+void calc_milk(milk_bucket A, milk_bucket B, milk_bucket C) {
+	int status;
 	int i;
-	milk_bucket A_L, B_L, C_L;
-	status_l = (A.milk<<10)+(B.milk<<5)+C.milk;  //记录状态
-	//cout << A.milk << " "  << B.milk << " "  <<  C.milk << endl;
+	milk_bucket A_L, B_L, C_L;  //用来存储以备还原
+	status = (A.milk<<10)+(B.milk<<5)+C.milk;  //记录状态
 	
-	flag = 1;
-	
-	for(i=0; i<len; i++)
-	{
-		if(status_l == status[i])//已出现过该状态
-		{
-			flag = 0;
-			return;
+	if(m[status]) return;//已出现过该状态
+	m[status] = true;
+	if(!A.milk) {  //如果A桶此时为空，则记录下c桶现在的奶量
+		c_milk[len_cmilk++] = C.milk;
+	}
+	if(A.milk) {
+		//A->B
+		A_L = A; B_L = B; C_L = C; //备份
+		if(B.milk!=B.capacity) {
+			pour_fromXtoY(&A, &B);
+			calc_milk(A, B, C);
 		}
+		A = A_L; B = B_L; C = C_L; //还原
+		//A->C
+		if(C.milk!=C.capacity)
+		{
+			pour_fromXtoY(&A, &C);
+			calc_milk(A, B, C);
+		}
+		A = A_L; B = B_L; C = C_L; //还原
 	}
 	
-	if(flag)//未出现过该状态
+	if(B.milk)
 	{
-		if(!A.milk)
-		{
-			c_milk[len_cmilk++] = C.milk;
+		A_L = A; B_L = B; C_L = C; //备份
+		//B->A
+		if(A.milk!=A.capacity) {
+			pour_fromXtoY(&B, &A);
+			calc_milk(A, B, C);
 		}
-		status[len++] = status_l;    //记录该状态
-		if(A.milk)
-		{
-			//A->B
-			A_L = A; B_L = B; C_L = C;
-			if(B.milk!=B.capacity)
-			{
-				pour_fromXtoY(&A, &B);
-				pour_milk(A, B, C);
-			}
-			A = A_L; B = B_L; C = C_L;
-			//A->C
-			if(C.milk!=C.capacity)
-			{
-				pour_fromXtoY(&A, &C);
-				pour_milk(A, B, C);
-			}
-			A = A_L; B = B_L; C = C_L;
+		A = A_L; B = B_L; C = C_L; //还原
+		//B->C
+		if(C.milk!=C.capacity) {
+			pour_fromXtoY(&B, &C);
+			calc_milk(A, B, C);
 		}
-		
-		if(B.milk)
-		{
-			A_L = A; B_L = B; C_L = C;
-			//B->A
-			if(A.milk!=A.capacity)
-			{
-				pour_fromXtoY(&B, &A);
-				pour_milk(A, B, C);
-			}
-			A = A_L; B = B_L; C = C_L;
-			//B->C
-			if(C.milk!=C.capacity)
-			{
-				pour_fromXtoY(&B, &C);
-				pour_milk(A, B, C);
-			}
-			A = A_L; B = B_L; C = C_L;
+		A = A_L; B = B_L; C = C_L; //还原
+	}
+	if(C.milk) {
+		A_L = A; B_L = B; C_L = C; //备份
+		//C->A
+		if(A.milk!=A.capacity) {
+			pour_fromXtoY(&C, &A);
+			calc_milk(A, B, C);
 		}
-		if(C.milk)
+		A = A_L; B = B_L; C = C_L; //还原
+		//C->B
+		if(B.milk!=B.capacity)
 		{
-			A_L = A; B_L = B; C_L = C; 
-			//C->A
-			if(A.milk!=A.capacity)
-			{
-				pour_fromXtoY(&C, &A);
-				pour_milk(A, B, C);
-			}
-			A = A_L; B = B_L; C = C_L;
-			//C->B
-			if(B.milk!=B.capacity)
-			{
-				pour_fromXtoY(&C, &B);
-				pour_milk(A, B, C);
-			}
-			A = A_L; B = B_L; C = C_L;
+			pour_fromXtoY(&C, &B);
+			calc_milk(A, B, C);
 		}
+		A = A_L; B = B_L; C = C_L; //还原
 	}
 }
 
@@ -129,7 +101,7 @@ int main()
 	//INIT
 	C.milk = C.capacity; 
 	A.milk = B.milk = 0;
-	pour_milk(A, B, C);
+	calc_milk(A, B, C);
 	sort(c_milk, c_milk+len_cmilk);
 	for(i=0; i<len_cmilk-1; i++)
 		fout<<c_milk[i]<<" ";
