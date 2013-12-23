ccce260391de1c046dc343fb180fff54d01d2f4c
Jacky Chen <jingchen@cmu.edu>
2013-12-23 12:35:03 -0800

    update runround.cpp

--- src/runround.cpp
+++ src/runround.cpp
@@ -5,55 +5,33 @@ LANG: C++
 */
 #include <fstream>
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-int N_char[9];
-bool digital[10], digital1[10];
 	
-bool is_rr(int n)
-{
-	int len = 0, i;
+bool is_rr(int n) {
+	int len = 0, i, N_char[9];
 	int n_tmp = n;
-	for(i=0; i<10; i++)
-	{
-		digital[i] = false;
-	}
-	while(n_tmp)
-	{
-		int dig_tmp;
-		dig_tmp = n_tmp%10;
-		if(digital[dig_tmp]||!dig_tmp) 
+	vector<bool> digital(10, false), digital1(10, false);
+	while(n_tmp) {  //把每位拆出来
+		int dig_tmp = n_tmp%10;
+		if(digital[dig_tmp] || !dig_tmp) 
 			return false; //重复了、或者dig_tmp为0
 		digital[dig_tmp] = true;
 		N_char[len++] = dig_tmp;
 		n_tmp/=10;
 	}
-	for(i=0; i<10; i++)
-	{
-		digital1[i] = false;
-	}
-	for(i=0; i<len/2; i++) //reverse the array
-	{
-		char tmp;
-		tmp = N_char[i];
-		N_char[i] = N_char[len-i-1];
-		N_char[len-i-1] = tmp;
-	}
+	reverse(N_char, N_char+len);//reverse the array
 	int cur = 0;
 	digital1[N_char[cur]] = true;
-	for(i=1; i<len; i++)
-	{
+	for(i=1; i<len; i++) {
 		cur = (cur+N_char[cur])%len;
 		if(digital1[N_char[cur]]) return false;
 		else
 			digital1[N_char[cur]] = true;
 	}
 	cur = (cur+N_char[cur])%len;
-	if(!cur)
-		return true;  //finally come back to first number
-	else 
-		return false;
+	return !cur;  //最后又回到起点
 }
 
 int main() {
@@ -62,11 +40,7 @@ int main() {
 	freopen("runround.in", "r", stdin);
     freopen("runround.out", "w", stdout);
 	cin>>N;   //freopen("CON", "w", stdout);
-
-	while(!is_rr(++N))
-	{
-		;
-	}
-	cout<<N<<endl;
+	while(!is_rr(++N)) {} //不断循环直至找到
+	cout << N << endl;
 	return 0;
 }
--- src/subset.cpp
+++ src/subset.cpp
@@ -3,9 +3,12 @@ ID:wo__che1
 PROG: subset
 LANG: C++
 */
+/*
+动态规划 设分成的子集为set1,set2,sum=(n*(n+1)div 2)div 2. 设f[i,j]表示取前i个数，使set1总数和为j的方案数.第i个数的值为i
+{其实就是01背包求方案数} 分析为什么它是0\1背包：将一个集合划分成 两个“元素总和相等”的集合，设原集合的元素总和为sum，则划分后的集合的元素综合都为sum/2。那么我们可以把sum/2看成背包的容量，原集合中的数字看为物品的重量及价值（这里价值维度可以淡化，就像装箱问题）。则原问题转化为 “从原集合中选出n个物品，使这n个物品恰好放满容量为sum/2的背包的方案总数”。
+*/
 #include <fstream>
 #include <iostream>
-
 using namespace std;
 
 unsigned int num[40][391];
@@ -16,10 +19,9 @@ int count(int n, int sum)
 	num[1][0] = 1;
 	
 	for(i=2; i<=n; i++)
-		for(j=0; j<=sum; j++)
-		{
+		for(j=0; j<=sum; j++) {
 			if(j-i>=0)
-				num[i][j] = num[i-1][j] + num[i-1][j-i];
+				num[i][j] = num[i-1][j] + num[i-1][j-i]; //不选i + 选i的方案数
 			else
 				num[i][j] = num[i-1][j];
 		}
@@ -31,10 +33,10 @@ int main() {
 	int N, i;
 	freopen("subset.in", "r", stdin);
     freopen("subset.out", "w", stdout);
-	cin>>N;   freopen("CON", "w", stdout);
-	if(N*(N+1)%4) 
-	{
-		cout<<"0"<<endl;
+	cin>>N;   //freopen("CON", "w", stdout);
+	//如果和不是偶数，则不存在这样的划分
+	if(N*(N+1)%4) {
+		cout << "0" << endl;
 	    return 0;
 	}
 	
